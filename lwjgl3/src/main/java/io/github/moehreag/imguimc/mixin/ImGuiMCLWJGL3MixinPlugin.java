ecd37801189b8995ea4fe3ccd54ff02bd411687b
Sophia Foxy Coxy <foxycoxy@thefoxesare.online>
2023-09-22 22:33:45 +1000

    fixes:tm

---
+++ lwjgl3/src/main/java/io/github/moehreag/imguimc/mixin/ImGuiMCLWJGL3MixinPlugin.java
@@ -0,0 +1,46 @@
+package io.github.moehreag.imguimc.mixin;
+
+import java.util.List;
+import java.util.Set;
+
+import io.github.moehreag.imguimc.ImGuiMC;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class ImGuiMCLWJGL3MixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return !ImGuiMC.LWJGL2;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
